e798f386769fdb6c5328ecb358393100eff5cfb2
anitkumarsingh <anikumarsingh99@gmail.com>
2023-03-11 16:59:04 +0530

    microfront-end integration

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,10 +1,19 @@
 const htmlWebpack = require('html-webpack-plugin');
+const moduleFedderationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
 module.exports = {
   mode:'development',
   devServer:{
     port:8081
   },
   plugins:[
+    new moduleFedderationPlugin({
+      name:'products',
+      filename:'remoteEntry.js',
+      exposes:{
+        './ProductsIndex':'./src/index'
+      }
+    }),
     new htmlWebpack({
       template:'./public/index.html'
     })
