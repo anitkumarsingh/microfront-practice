ed3e6a5276886c8bfa8d9b2b8205f40daa646941
anitkumarsingh <anikumarsingh99@gmail.com>
2023-03-11 17:50:49 +0530

    cart

---
+++ cart/webpack.config.js
@@ -0,0 +1,21 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports ={
+  mode:'development',
+  devServer:{
+    port:8082
+  },
+  plugins:[
+    new ModuleFederationPlugin({
+      name:'cart',
+      filename:'cartEntry.js',
+      exposes:{
+        './showCart':'./src/index.js'
+      }
+    }),
+    new htmlWebpackPlugin({
+      template:'./public/index.html'
+    })
+  ]
+}
